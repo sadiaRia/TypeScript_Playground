a15d1a55f99e776a8134f6b68cf898bf179fb0d7
sadia <sadia@sheba.xyz>
2024-10-05 12:24:03 +0600

    type any

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-let age :  number = 20;
-if(age < 50)
-    age += 10;
+// let age :  number = 20;
+// if(age < 50)
+//     age += 10;
 
-console.log(age);
+// console.log(age);
+
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level; //we should avoid any type
+level = 1;
+level = 'a';
+render(1);
+function render( document: any){
+    console.log(document);
+}
